bd0a5f95249f3983b2c270f2bf88676d6190f285
Muhammad Linggar Ryanidha <Muhammad Linggar Ryanidha>
2023-12-08 20:35:15 +0700

    fix path problem

--- js/fetchFirstPage.js
+++ js/fetchFirstPage.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    fetch('../../pages/latest.html')
+    fetch('../pages/latest.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('first-page').innerHTML = data;
